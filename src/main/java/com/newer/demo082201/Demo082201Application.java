5c8b43120eceab8419bd5cf81deabba21bf56482
lihaoliang27 <754821499@qq.com>
2019-08-22 15:13:17 +0800

    create project

---
+++ src/main/java/com/newer/demo082201/Demo082201Application.java
@@ -0,0 +1,13 @@
+package com.newer.demo082201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo082201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo082201Application.class, args);
+    }
+
+}
